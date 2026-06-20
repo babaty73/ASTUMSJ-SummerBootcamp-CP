b14c9dcc183b02e000cbb46a64cb9b1c03e3d9cc
imran idris <imranidris10999@gmail.com>
2026-06-20 19:23:02 +0300

    Solved a problem

---
+++ Partition array.py
@@ -0,0 +1,8 @@
+#leetcode problem 2161 partition according to array
+class Solution:
+    def pivotArray(self, nums: list[int], pivot: int) -> list[int]:
+        less = [x for x in nums if x < pivot]
+        equal = [x for x in nums if x == pivot]
+        greater = [x for x in nums if x > pivot]
+        
+        return less + equal + greater
