1f84283369dd2bb491cdb5800cc9896d0ec1d877
imran <imranidris10999@gmail.com>
2026-06-25 22:48:18 -0700

    solved a problem

---
+++ slimes on line.py
@@ -0,0 +1,14 @@
+t = int(input())
+
+for i in range(t):
+    n = int(input())
+    count = 0
+    arrs = map(int, input().split())
+    arr = sorted(arrs)
+    low = arr[0]
+    high = arr[-1]
+    x = (high - low + 1) // 2
+    
+    count += x
+    
+    print(count)
