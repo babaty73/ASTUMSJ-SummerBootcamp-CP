093d51316ad537a8bb80f49d647372b65af9ed30
imran idris <imranidris10999@gmail.com>
2026-07-09 22:57:03 +0300

    Solved a problem

---
+++ Simply sitting on chairs.py
@@ -0,0 +1,9 @@
+#codeforces problem 2210 simply sitting on chairs 
+for i in range (int(input())):
+    n = int(input())
+    arr = list(map(int, input().split()))
+    count = 0
+    for i in range(n):
+        if i+1 >= arr[i] :
+            count+=1
+    print(count)
